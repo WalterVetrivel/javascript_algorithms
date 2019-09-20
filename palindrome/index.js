499ca1be71c4a172c72a499196fb31ed0bc816ee
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-09-20 23:26:52 +1000

    Added an alternate solution to palindrome using every()

--- palindrome/index.js
+++ palindrome/index.js
@@ -14,17 +14,18 @@ function reverseString(str) {
 function palindromeLoop(str) {
 	let i = 0,
 		j = str.length - 1;
-	while (i < j) {
-		if (str[i] !== str[j]) return false;
-		i++;
-		j--;
-	}
+	while (i < j) if (str[i++] !== str[j--]) return false;
 	return true;
+}
+
+function palindromeEvery(str) {
+	return str.split('').every((c, i) => c === str[str.length - i - 1]);
 }
 
 function palindrome(str) {
 	// return str === reverseString(str);
 	return palindromeLoop(str);
+	// return palindromeEvery(str);
 }
 
 module.exports = palindrome;
