f5cb644ae3ebc44481c5d7dcb9dc8ebee0f272ea
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-09-21 00:15:19 +1000

    Finished maxchar basic implementation

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,33 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxCharBasic(str) {
+	const sorted = str.split('').sort();
+
+	let max = 1,
+		count = 1,
+		char = sorted[0],
+		maxChar = char;
+
+	for (let i = 1; i < sorted.length - 2; i++) {
+		if (sorted[i] === char) {
+			count++;
+		} else {
+			if (count >= max) {
+				maxChar = char;
+				max = count;
+			}
+
+			char = sorted[i];
+			count = 1;
+		}
+	}
+
+	return maxChar;
+}
+
+function maxChar(str) {
+	return maxCharBasic(str);
+}
 
 module.exports = maxChar;
