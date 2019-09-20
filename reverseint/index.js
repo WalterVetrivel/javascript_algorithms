370c73ccb14a58690ec1714db01380fdb10738b4
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-09-20 23:48:28 +1000

    Finished reverseint

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,30 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseString(str) {
+	return str.split('').reduce((res, c) => c + res, '');
+}
+
+function reverseIntAsString(n) {
+	let numStr = Math.abs(n) + '';
+	const mul = n < 0 ? -1 : 1;
+	numStr = reverseString(numStr);
+	return parseInt(numStr) * mul;
+}
+
+function reverseIntAsNum(n) {
+	let absNum = Math.abs(n);
+	let res = 0;
+	let mul = n < 0 ? -1 : 1;
+	while (absNum > 0) {
+		res = res * 10 + (absNum % 10);
+		absNum = Math.floor(absNum / 10);
+	}
+	return res * mul;
+}
+
+function reverseInt(n) {
+	return reverseIntAsNum(n);
+}
 
 module.exports = reverseInt;
