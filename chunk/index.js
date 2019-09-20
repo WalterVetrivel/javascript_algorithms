46b61b162bc7be2ad2c7b4569e7bbf596a9f9e4d
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-09-21 01:28:00 +1000

    Implemented chunk

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunkSlice(array, size) {
+	let chunkCount = Math.ceil(array.length / size);
+	const result = [];
+	let startIndex = 0;
+
+	while (chunkCount > 0) {
+		result.push(array.slice(startIndex, startIndex + size));
+		startIndex += size;
+		chunkCount--;
+	}
+
+	return result;
+}
+
+function chunkArray(array, size) {
+	const chunked = [];
+
+	for (let el of array) {
+		let lastChunk = chunked[chunked.length - 1];
+
+		if (!lastChunk || lastChunk.length === size) {
+			chunked.push([el]);
+		} else {
+			lastChunk.push(el);
+		}
+	}
+
+	return chunked;
+}
+
+function chunk(array, size) {
+	// return chunkSlice(array, size);
+	return chunkArray(array, size);
+}
 
 module.exports = chunk;
