b763b39c27e6d0986af098870346eec38bcb7e12
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-09-21 23:30:49 +1000

    Implemented anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,55 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function generateCharacterMap(str) {
+	/* return str.split('').reduce((charMap, char) => {
+		charMap[char] = charMap[char] + 1 || 1;
+		return charMap;
+	}, {}); */
+	let map = {};
+
+	for (let c of str) {
+		map[c] = map[c] + 1 || 1;
+	}
+
+	return map;
+}
+
+function sortString(str) {
+	return str
+		.split('')
+		.sort()
+		.join('');
+}
+
+function removeSpecialCharacters(str) {
+	return str.replace(/[^\w]/g, '');
+}
+
+function compareMaps(mapA, mapB) {
+	if (Object.keys(mapA).length !== Object.keys(mapB).length) return false;
+
+	for (let key in mapA) {
+		if (!mapA.hasOwnProperty(key) || mapB[key] !== mapA[key]) return false;
+	}
+
+	return true;
+}
+
+function anagrams(stringA, stringB) {
+	/* return (
+		sortString(removeSpecialCharacters(stringA).toLowerCase()) ===
+		sortString(removeSpecialCharacters(stringB).toLowerCase())
+	); */
+	const mapA = generateCharacterMap(
+		removeSpecialCharacters(stringA).toLowerCase()
+	);
+
+	const mapB = generateCharacterMap(
+		removeSpecialCharacters(stringB).toLowerCase()
+	);
+
+	return compareMaps(mapA, mapB);
+}
 
 module.exports = anagrams;
