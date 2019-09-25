c69f7d029646564a8dd6d62cfc0e19b23660a0a2
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-09-26 01:42:30 +1000

    Added more solutions for steps

--- steps/index.js
+++ steps/index.js
@@ -21,10 +21,60 @@ function generateStep(stepChar, lineNum, length) {
 	return stepChar.repeat(lineNum) + ' '.repeat(length - lineNum);
 }
 
+function generateStepPad(stepChar, lineNum, length) {
+	return stepChar.repeat(lineNum).padEnd(length);
+}
+
+function stepsIterative(n) {
+	for (let i = 1; i <= n; i++) {
+		step = '';
+		for (let j = 1; j <= n; j++) {
+			if (j <= i) {
+				step += '#';
+			} else {
+				step += ' ';
+			}
+		}
+		console.log(step);
+	}
+}
+
+function stepsRecursive(length, stepChar = '#', lineNum = 1) {
+	if (lineNum === length) {
+		console.log(stepChar.repeat(length));
+		return;
+	}
+
+	console.log(stepChar.repeat(lineNum).padEnd(length));
+
+	stepsRecursive(length, stepChar, lineNum + 1);
+}
+
+function stepsRecursiveAlt(n, row = 0, step = '') {
+	if (row === n) return;
+
+	if (n === step.length) {
+		console.log(step);
+		return stepsRecursiveAlt(n, row + 1);
+	}
+
+	if (step.length <= row) {
+		step += '#';
+	} else {
+		step += ' ';
+	}
+
+	return stepsRecursiveAlt(n, row, step);
+}
+
 function steps(n) {
 	for (let i = 1; i <= n; i++) {
-		console.log(generateStep('#', i, n));
+		// console.log(generateStep('#', i, n));
+		// console.log(generateStepPad('#', i, n));
 	}
+	// stepsIterative(n);
+	// stepsRecursive(n);
+	stepsRecursiveAlt(n);
 }
 
 module.exports = steps;
