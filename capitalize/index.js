7c306207dba55a429fb985599a0dd3057fdc4883
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-09-26 01:06:12 +1000

    Added brute force solutions to capitalize

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,16 +7,42 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
+function capitalizeBruteForce(str) {
+	let result = str[0].toUpperCase();
+
+	for (let i = 1; i < str.length; i++) {
+		result += str[i - 1] === ' ' ? str[i].toUpperCase() : str[i];
+	}
+
+	return result;
+}
+
+function capitalizeBruteForceReduce(str) {
+	return str
+		.split('')
+		.reduce(
+			(result, c, i) =>
+				i === 0 || result.endsWith(' ') ? result + c.toUpperCase() : result + c,
+			''
+		);
+}
+
 function capitalizeRegExp(str) {
 	return str.replace(/^[a-z]|\s[a-z]/g, val => val.toUpperCase());
 }
 
-function capitalize(str) {
-	/* return str
+function capitalizeSplitMap(str) {
+	return str
 		.split(' ')
 		.map(word => word[0].toUpperCase() + word.slice(1))
-		.join(' '); */
+		.join(' ');
+}
+
+function capitalize(str) {
+	// return capitalizeSplitMap(str);
 	return capitalizeRegExp(str);
+	// return capitalizeBruteForce(str);
+	// return capitalizeBruteForceReduce(str);
 }
 
 module.exports = capitalize;
