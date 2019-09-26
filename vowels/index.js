c04bc98ee44bf0625a99aa128c5efdb7acdf3fca
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-09-26 21:31:58 +1000

    Added solutions for vowels

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,29 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function getVowelsRegex(str) {
+	const vowelRegex = /[aeiou]/g;
+	return str.toLowerCase().match(vowelRegex);
+}
+
+function vowelsIterative(str) {
+	let vowelCount = 0;
+	let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+	for (let char of str.toLowerCase().replace(/[^a-z]/g, '')) {
+		if (vowels.indexOf(char) >= 0) vowelCount++;
+	}
+
+	return vowelCount;
+}
+
+function vowels(str) {
+	/* if (str) {
+		const vowels = getVowelsRegex(str);
+		if (vowels) return vowels.length;
+	}
+	return 0; */
+	return vowelsIterative(str);
+}
 
 module.exports = vowels;
