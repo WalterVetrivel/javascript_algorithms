105aca2cf6c3e4fd015a590b9e4ab804d91a3d9b
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-09-26 19:36:58 +1000

    Added solution for pyramid

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,40 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramidBruteForce(n) {
+	let level = '';
+	for (let i = 1; i <= n; i++) {
+		for (let j = 1; j <= n - i; j++) {
+			level += ' ';
+		}
+		for (let j = 0; j < i * 2 - 1; j++) {
+			level += '#';
+		}
+		for (let j = 1; j <= n - i; j++) {
+			level += ' ';
+		}
+		console.log(level);
+		level = '';
+	}
+}
+
+function pyramidLevel(level, size) {
+	return (
+		' '.repeat(size - level) +
+		'#'.repeat(2 * level - 1) +
+		' '.repeat(size - level)
+	);
+}
+
+function pyramidRepeat(n) {
+	for (let i = 1; i <= n; i++) {
+		console.log(pyramidLevel(i, n));
+	}
+}
+
+function pyramid(n) {
+	// pyramidBruteForce(n);
+	pyramidRepeat(n);
+}
 
 module.exports = pyramid;
